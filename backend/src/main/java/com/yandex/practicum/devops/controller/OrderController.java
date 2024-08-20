78259c2ace2ad42206455b1e330923cf3319049f
Ggosh <radiging@gmail.com>
2024-08-20 23:46:10 +0300

    bebra1

--- backend/src/main/java/com/yandex/practicum/devops/controller/OrderController.java
+++ backend/src/main/java/com/yandex/practicum/devops/controller/OrderController.java
@@ -1,4 +1,4 @@
-package com.yandex.practicum.devops.controller;
+epackage com.yandex.practicum.devops.controller;
 
 import com.yandex.practicum.devops.dto.OrderProductDto;
 import com.yandex.practicum.devops.exception.ResourceNotFoundException;
